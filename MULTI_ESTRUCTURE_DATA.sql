3cdd8a20bf137b111fa62eb2c99e4a69004a7c90
requiemrobert <robertsoft2718@gmail.com>
2017-11-27 15:48:30 -0400

    data base final

--- MULTI_ESTRUCTURE_DATA.sql
+++ MULTI_ESTRUCTURE_DATA.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : global_system
+Source Server         : MULTI_ONA_GRAF
 Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : multi
+Source Host           : 127.0.0.1:3306
+Source Database       : multi911
 
 Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-11-23 17:01:58
+Date: 2017-11-27 14:26:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `autorizacion` (
   KEY `id_modulo_fk` (`id_modulo_fk`),
   CONSTRAINT `autorizacion_ibfk_1` FOREIGN KEY (`id_perfil_fk`) REFERENCES `perfil` (`id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `autorizacion_ibfk_2` FOREIGN KEY (`id_modulo_fk`) REFERENCES `modulo` (`id_modulo`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of autorizacion
@@ -39,6 +39,32 @@ INSERT INTO `autorizacion` VALUES ('2', '1', '2', '1');
 INSERT INTO `autorizacion` VALUES ('3', '1', '3', '1');
 INSERT INTO `autorizacion` VALUES ('4', '1', '4', '1');
 INSERT INTO `autorizacion` VALUES ('5', '1', '5', '1');
+INSERT INTO `autorizacion` VALUES ('6', '1', '6', '1');
+INSERT INTO `autorizacion` VALUES ('7', '1', '7', '1');
+
+-- ----------------------------
+-- Table structure for cliente
+-- ----------------------------
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `cod_cliente` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `pref_ci_rif` varchar(5) DEFAULT NULL,
+  `ci_rif` int(11) NOT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `correo` varchar(150) DEFAULT NULL,
+  `direccion` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`cod_cliente`,`ci_rif`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of cliente
+-- ----------------------------
+INSERT INTO `cliente` VALUES ('1', 'robert', 'garcia', 'V', '20914733', '2128626341', 'rogarcia@hotmail.com', 'la candelaria');
+INSERT INTO `cliente` VALUES ('2', 'robert', 'pani', 'V', '21914733', '2128626341', '', '');
+INSERT INTO `cliente` VALUES ('3', 'robert', 'garcia', 'V', '22914733', '2128626341', 'rogarcia@hotmail.com', 'la candelaria');
+INSERT INTO `cliente` VALUES ('4', 'juan ', 'perez', 'J', '38054588', '2128626341', 'jperez91@hotmail.com', 'la candelaria');
 
 -- ----------------------------
 -- Table structure for modulo
@@ -51,16 +77,55 @@ CREATE TABLE `modulo` (
   `activo` tinyint(10) NOT NULL,
   PRIMARY KEY (`id_modulo`,`id_modulo_fk`),
   KEY `id_modulo` (`id_modulo`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of modulo
 -- ----------------------------
-INSERT INTO `modulo` VALUES ('1', '0', 'Produccion', '1');
-INSERT INTO `modulo` VALUES ('2', '0', 'Inventario', '1');
-INSERT INTO `modulo` VALUES ('3', '0', 'Ventas', '1');
-INSERT INTO `modulo` VALUES ('4', '0', 'Estadisticas', '1');
-INSERT INTO `modulo` VALUES ('5', '0', 'Clientes', '1');
+INSERT INTO `modulo` VALUES ('1', '0', 'Registrar_Cliente', '1');
+INSERT INTO `modulo` VALUES ('2', '0', 'Registrar_Pedidos', '1');
+INSERT INTO `modulo` VALUES ('3', '0', 'Consultar_Pedidos', '1');
+INSERT INTO `modulo` VALUES ('4', '0', 'Registrar_Inventario', '1');
+INSERT INTO `modulo` VALUES ('5', '0', 'Listar_Inventario', '1');
+INSERT INTO `modulo` VALUES ('6', '0', 'Prudctos_En_Proceso', '1');
+INSERT INTO `modulo` VALUES ('7', '0', 'Registrar_Pedidos', '1');
+INSERT INTO `modulo` VALUES ('8', '0', 'listar_Clientes', '1');
+
+-- ----------------------------
+-- Table structure for pedidos
+-- ----------------------------
+DROP TABLE IF EXISTS `pedidos`;
+CREATE TABLE `pedidos` (
+  `numero_orden` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre_cliente` varchar(100) DEFAULT NULL,
+  `cod_cliente_fk` int(10) DEFAULT NULL,
+  `tipo_pieza` varchar(10) DEFAULT NULL,
+  `cod_pieza` varchar(10) NOT NULL,
+  `descripcion` varchar(150) DEFAULT NULL,
+  `marca_fabricante` varchar(20) DEFAULT NULL,
+  `fecha_pedido` date DEFAULT NULL,
+  `estatus` varchar(20) DEFAULT NULL,
+  `fec_estatus` date DEFAULT NULL,
+  PRIMARY KEY (`numero_orden`)
+) ENGINE=InnoDB AUTO_INCREMENT=3910 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of pedidos
+-- ----------------------------
+INSERT INTO `pedidos` VALUES ('3896', 'robert garcia', '1', 'Retrovisor', '14', 'prueba', 'toyota', '2017-11-27', 'listo', '2017-11-27');
+INSERT INTO `pedidos` VALUES ('3897', 'robert garcia', '1', 'Retrovisor', '15', '', 'toyota', '2017-11-08', 'en proceso', '2017-11-27');
+INSERT INTO `pedidos` VALUES ('3898', 'robert garcia', '1', 'Parachoque', '16', '', 'toyota', '2017-11-15', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3899', 'robert garcia', '1', 'Capo', '17', '', 'toyota', '2017-11-16', 'en espera de repuest', '2017-11-27');
+INSERT INTO `pedidos` VALUES ('3900', 'robert garcia', '1', 'Techo', '18', '', 'toyota', '2017-11-13', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3901', 'robert garcia', '1', 'Capo', '19', '', 'toyota', '2017-11-14', 'pausado', '2017-11-27');
+INSERT INTO `pedidos` VALUES ('3902', 'robert garcia', '1', 'Retrovisor', '20', '', 'toyota', '2017-10-17', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3903', 'robert garcia', '1', 'Retrovisor', '21', '', 'toyota', '2017-10-18', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3904', 'robert garcia', '1', 'Techo', '22', '', 'toyota', '2017-10-04', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3905', 'robert garcia', '1', 'Capo', '23', '', 'toyota', '2017-09-28', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3906', 'juan  perez', '4', 'Retrovisor', '24', '', 'toyota', '2017-10-11', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3907', 'juan  perez', '4', 'Capo', '25', '', 'toyota', '2017-10-19', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3908', 'robert garcia', '1', 'Retrovisor', '26', 'graficas', 'toyota', '2017-12-06', 'pendiente', null);
+INSERT INTO `pedidos` VALUES ('3909', 'robert garcia', '1', 'Techo', '27', '', 'toyota', '2017-12-14', 'pendiente', null);
 
 -- ----------------------------
 -- Table structure for perfil
@@ -84,63 +149,35 @@ INSERT INTO `perfil` VALUES ('2', 'usuario', '1');
 -- ----------------------------
 DROP TABLE IF EXISTS `pieza`;
 CREATE TABLE `pieza` (
-  `id_pieza` int(10) NOT NULL AUTO_INCREMENT,
+  `cod_pieza` int(10) NOT NULL AUTO_INCREMENT,
+  `numero_orden_fk` varchar(10) DEFAULT NULL,
   `tipo_pieza` varchar(50) NOT NULL,
-  `fabricante` varchar(50) DEFAULT NULL,
+  `marca_fabricante` varchar(50) DEFAULT NULL,
   `precio_pieza` varchar(15) DEFAULT NULL,
-  `numero_pieza` varchar(10) DEFAULT NULL,
-  `descripcion_pieza` varchar(150) DEFAULT NULL,
-  `fec_produccion` date DEFAULT NULL,
-  PRIMARY KEY (`id_pieza`)
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=latin1;
+  `descripcion` varchar(150) DEFAULT NULL,
+  `fec_estatus` date DEFAULT NULL,
+  `cod_cliente_fk` int(10) DEFAULT NULL,
+  `estatus` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`cod_pieza`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of pieza
 -- ----------------------------
-INSERT INTO `pieza` VALUES ('5', 'Retrovisor', 'toyota', '150000', 'ret322', 'retrovisor', '2017-11-07');
-INSERT INTO `pieza` VALUES ('6', 'Parachoques', 'toyota', '350000', 'pc3322', 'para choques delantero ', '2017-11-07');
-INSERT INTO `pieza` VALUES ('7', 'Capo', 'toyota', '450000', 'c2333', '', '2017-10-30');
-INSERT INTO `pieza` VALUES ('8', 'Techo', 'toyota', '1500000', 't1125', '', '2017-10-31');
-INSERT INTO `pieza` VALUES ('16', 'Retrovisor', 'toyota', '123302', 'ret213', 'retrovisor clÃ¡sico', '2017-10-10');
-INSERT INTO `pieza` VALUES ('17', 'Retrovisor', 'toyota', '25000', 'ret214', 'retrovisor ', '2017-10-12');
-INSERT INTO `pieza` VALUES ('18', 'Retrovisor', 'toyota', '25000', 'ret21465', 'retrovisor ', '2017-10-24');
-INSERT INTO `pieza` VALUES ('19', 'Retrovisor', 'toyota', '50000', 'ret21433', 'retrovisor ', '2017-10-20');
-INSERT INTO `pieza` VALUES ('20', 'Retrovisor', 'toyota', '350000', 'ret21433', 'retrovisor ', '2017-10-19');
-INSERT INTO `pieza` VALUES ('21', 'Retrovisor', 'toyota', '350000', 'ret21433', 'retrovisor ', '2017-10-25');
-INSERT INTO `pieza` VALUES ('22', 'Retrovisor', 'toyota', '350000', 'ret21433', 'retrovisor ', '2017-11-08');
-INSERT INTO `pieza` VALUES ('23', 'Retrovisor', 'toyota', '350000', 'ret21432', 'retrovisor ', '2017-11-09');
-INSERT INTO `pieza` VALUES ('24', 'Retrovisor', 'toyota', '350000', 'ret21435', 'retrovisor ', '2017-11-10');
-INSERT INTO `pieza` VALUES ('25', 'Capo', 'toyota', '450000', 'c23333', '', '2017-09-20');
-INSERT INTO `pieza` VALUES ('26', 'Capo', 'toyota', '450000', 'c23342', '', '2017-10-09');
-INSERT INTO `pieza` VALUES ('27', 'Capo', 'toyota', '450000', 'c23123', '', '2017-09-27');
-INSERT INTO `pieza` VALUES ('28', 'Capo', 'toyota', '450000', 'c23353', '', '2017-10-12');
-INSERT INTO `pieza` VALUES ('29', 'Parachoques', 'toyota', '1400000', 'pch1233', 'parachoques 1', '2017-10-09');
-INSERT INTO `pieza` VALUES ('30', 'Parachoques', 'toyota', '1400000', 'pch1233', 'parachoques 1', '2017-10-04');
-INSERT INTO `pieza` VALUES ('31', 'Parachoques', 'toyota', '1400000', 'pch1233', 'parachoques 1', '2017-11-06');
-INSERT INTO `pieza` VALUES ('32', 'Parachoques', 'toyota', '1400000', 'pch1233', 'parachoques 1', '2017-11-14');
-INSERT INTO `pieza` VALUES ('33', 'Parachoques', 'toyota', '1400000', 'pch123311', 'parachoques 1', '2017-10-16');
-INSERT INTO `pieza` VALUES ('34', 'Parachoques', 'toyota', '1400000', 'pch123311', 'parachoques 123456', '2017-10-25');
-INSERT INTO `pieza` VALUES ('35', 'Parachoques', 'toyota', '1400000', 'pch123311', 'parachoques 123456', '2017-09-26');
-INSERT INTO `pieza` VALUES ('36', 'Parachoques', 'toyota', '1400000', 'pch123311', 'parachoques 123456', '2017-11-05');
-INSERT INTO `pieza` VALUES ('37', 'Techo', 'toyota', '500000', 't1233', 'techo verde', '2017-11-14');
-INSERT INTO `pieza` VALUES ('38', 'Capo', 'toyota', '1500000', 'ca1022', '', '2017-11-10');
-INSERT INTO `pieza` VALUES ('39', 'Techo', 'toyota', '500000', 'te422', '', '2017-11-09');
-INSERT INTO `pieza` VALUES ('42', 'Retrovisor', 'toyota', '1340000', '1223', '', '2017-09-19');
-INSERT INTO `pieza` VALUES ('43', 'Retrovisor', 'toyota', '1340000', '1223', '', '2017-09-19');
-INSERT INTO `pieza` VALUES ('44', 'Retrovisor', 'toyota', '1340000', '1223', '', '2017-09-19');
-INSERT INTO `pieza` VALUES ('53', 'Parachoques', 'toyota', '400000', '1233', '12121', '0000-00-00');
-INSERT INTO `pieza` VALUES ('54', 'Retrovisor', 'toyota', '350000', 'hu123', '', '2017-10-11');
-INSERT INTO `pieza` VALUES ('58', 'Parachoques', 'toyota', '350000', 'hu123', '', '2017-10-11');
-INSERT INTO `pieza` VALUES ('59', 'Parachoques', 'toyota', '350000', 'hu123', '', '2017-10-11');
-INSERT INTO `pieza` VALUES ('60', 'Parachoques', 'toyota', '350000', 'hu123', '', '2017-10-11');
-INSERT INTO `pieza` VALUES ('63', 'Retrovisor', 'toyota', '2500000', 'de123', '', '2017-12-20');
-INSERT INTO `pieza` VALUES ('64', 'Retrovisor', 'toyota', '15000', '122', '', '2017-12-08');
-INSERT INTO `pieza` VALUES ('65', 'Retrovisor', 'toyota', '', '123', '', '2017-12-06');
-INSERT INTO `pieza` VALUES ('66', 'Retrovisor', 'toyota', '180000', 'retro3222', '', '2017-10-18');
-INSERT INTO `pieza` VALUES ('67', 'Retrovisor', 'toyota', '150000', 'e23121', '', '2017-11-15');
-INSERT INTO `pieza` VALUES ('68', 'Techo', 'toyota', '15222', 'qsqsqs', '', '2017-12-06');
-INSERT INTO `pieza` VALUES ('69', 'Techo', 'toyota', '15222', 'qsqsqs', '', '2017-12-06');
-INSERT INTO `pieza` VALUES ('70', 'Techo', 'toyota', '15222', 'qsqsqs', '', '2017-12-06');
+INSERT INTO `pieza` VALUES ('14', '3896', 'Retrovisor', 'toyota', '1580000', null, '2017-11-27', '1', 'listo');
+INSERT INTO `pieza` VALUES ('15', '3897', 'Retrovisor', 'toyota', '', null, '2017-11-27', '1', 'en proceso');
+INSERT INTO `pieza` VALUES ('16', '3898', 'Parachoques', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('17', '3899', 'Capo', 'toyota', '', null, '2017-11-27', '1', 'en espera de repuestos');
+INSERT INTO `pieza` VALUES ('18', '3900', 'Techo', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('19', '3901', 'Capo', 'toyota', '', null, '2017-11-27', '1', 'pausado');
+INSERT INTO `pieza` VALUES ('20', '3902', 'Retrovisor', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('21', '3903', 'Retrovisor', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('22', '3904', 'Techo', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('23', '3905', 'Capo', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('24', '3906', 'Retrovisor', 'toyota', null, null, null, '4', 'pendiente');
+INSERT INTO `pieza` VALUES ('25', '3907', 'Capo', 'toyota', null, null, null, '4', 'pendiente');
+INSERT INTO `pieza` VALUES ('26', '3908', 'Retrovisor', 'toyota', null, null, null, '1', 'pendiente');
+INSERT INTO `pieza` VALUES ('27', '3909', 'Techo', 'toyota', null, null, null, '1', 'pendiente');
 
 -- ----------------------------
 -- Table structure for usuario
